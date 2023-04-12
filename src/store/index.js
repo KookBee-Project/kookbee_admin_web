700fb19bcb8563a5bf6f650f395b1de1b799616b
pshoon25 <pshoon25@naver.com>
2023-04-12 17:41:46 +0900

    #3_230412_2 feature/Admin/SignUp

--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
+import SignUpSlice from "./admin/SignUpSlice";
 
 export default configureStore({
-  reducer: {},
+  reducer: {
+    signUp: SignUpSlice,
+  },
 });
