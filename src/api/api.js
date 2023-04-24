52e0bd5b5481c4a56c67d03cab9bf41ede9033c1
HanwheeKim <84313936+KimHanWhee@users.noreply.github.com>
2023-04-24 10:32:34 +0900

    Update api.js

--- src/api/api.js
+++ src/api/api.js
@@ -3,9 +3,11 @@ axios.defaults.baseURL = "http://34.64.69.238:8000";
 axios.defaults.withCredentials = true;
 export const api = async (method, url, data) => {
   const refreshToken = localStorage.getItem("refreshToken");
+  const accessToken = localStorage.getItem("accessToken");
   const headers = refreshToken
     ? {
         RefreshToken: refreshToken,
+        AccesToken: accessToken,
       }
     : {};
   const response = await axios({
